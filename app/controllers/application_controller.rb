94dcbbf50dc318c3db1a5221b2c4fcdf8e12caaf
gabystern <stern.gabrielle@gmail.com>
2017-06-20 14:42:29 -0400

    working with authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+
+  def authorize_user!
+    if !current_user.present?
+      render json: {error: 'no user id present'}
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: user_id)
+  end
+
+  def user_id
+    request.headers['Authorization']
+  end
+
 end
